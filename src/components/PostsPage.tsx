187e16d60a7ed4698890f8cbb6f141ddf80e3239
Ilja-Prihach <piratehnik9508@gmail.com>
2026-02-14 18:47:24 +0300

    errors

--- src/components/PostsPage.tsx
+++ src/components/PostsPage.tsx
@@ -6,18 +6,22 @@ type Post = {
     title: string;
 }
 
+let retryCount = 0;
+
 function getPosts() {
+    retryCount++
+    console.log(retryCount)
+    if (true) {
+        throw new Error(" Test Error")
+    }
     return api.get<Post[]>("/posts").then((res) => res.data);
 }
 
 export const PostsPage = () => {
     const {
         data: posts,
-        isLoading,
-        isPending,
-        isFetching,
-        status,
-        fetchStatus
+        error,
+        isError
     } = useQuery({
         queryKey: ["posts"],
         queryFn: getPosts,
@@ -27,11 +31,8 @@ export const PostsPage = () => {
 
   return (
     <div className="flex flex-col gap-4">
-        {status}
-        {fetchStatus}
-        {isLoading && <div>Loading...</div>}
-        {isPending && <div>Pending...</div>}
-        {isFetching && <div>Fetching...</div>}
+        {isError && <div> Error: {error.message}</div>}
+        {retryCount}
         {posts?.map((post) => (
             <div key={post.id}>
                 {post.id}.{post.title}
