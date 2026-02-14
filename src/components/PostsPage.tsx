bd2075320c683c24b966feca9e628b10167a005a
Ilja-Prihach <piratehnik9508@gmail.com>
2026-02-14 18:53:29 +0300

    skiptoken

--- src/components/PostsPage.tsx
+++ src/components/PostsPage.tsx
@@ -1,4 +1,4 @@
-import {useQuery} from "@tanstack/react-query";
+import {skipToken, useQuery} from "@tanstack/react-query";
 import {api} from "../api/api.ts";
 
 type Post = {
@@ -6,33 +6,27 @@ type Post = {
     title: string;
 }
 
-let retryCount = 0;
 
 function getPosts() {
-    retryCount++
-    console.log(retryCount)
-    if (true) {
-        throw new Error(" Test Error")
-    }
     return api.get<Post[]>("/posts").then((res) => res.data);
 }
 
 export const PostsPage = () => {
+    const isAuth = false
+
+
     const {
         data: posts,
-        error,
-        isError
     } = useQuery({
         queryKey: ["posts"],
-        queryFn: getPosts,
+        queryFn: isAuth ? getPosts : skipToken,
         staleTime: 5000,
+
     });
 
 
   return (
     <div className="flex flex-col gap-4">
-        {isError && <div> Error: {error.message}</div>}
-        {retryCount}
         {posts?.map((post) => (
             <div key={post.id}>
                 {post.id}.{post.title}
